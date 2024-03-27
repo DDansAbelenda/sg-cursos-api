7ad4b8eb37f73184c37daffd1af516776143cbbd
Daniel Dans Abelenda <ddansabelenda@gmail.com>
2024-03-27 13:13:26 -0400

    modificando el factory de course

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -18,8 +18,25 @@ class CourseFactory extends Factory
     {   
         $faker = \Faker\Factory::create('es_ES');
 
+        $courseNames = [
+            'Introducción a la Programación',
+            'Desarrollo Web con HTML y CSS',
+            'Bases de Datos Relacionales',
+            'Inteligencia Artificial y Aprendizaje Automático',
+            'Diseño Gráfico y Multimedia',
+            'Inglés para Negocios',
+            'Gestión de Proyectos Ágiles',
+            'Marketing Digital',
+            'Fundamentos de Finanzas',
+            'Gestión del Talento Humano',
+            'Inglés A1',
+            'Inglés A2',
+            'Inglés B1',
+            'Inglés B2'
+        ];
+
         return [
-            'name' => $faker->name,
+            'name' => $faker->unique()->randomElement($courseNames),
             'description' => $faker->paragraph,
             'number_hours' => $faker->numberBetween(10, 100),
             'cost' => $faker->randomFloat(2, 50, 500),
